8aa4949ed7c66df79a4b028320a06f7eb3af4441
vikash sirvi <50855117+Vikashsirvi@users.noreply.github.com>
2021-11-02 22:14:02 +0530

    Component updates in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const onClickHandler = () => {
+    setShowParagraph(!showParagraph);
+  };
   return (
     <div className="app">
+      <Button onClick={onClickHandler}>Click here!</Button>
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={onClickHandler}>Click here!</Button>
     </div>
   );
 }
