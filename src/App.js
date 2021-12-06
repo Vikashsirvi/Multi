0235370e370027b7ac8225d2a354ab32b5c6e96f
vikash sirvi <50855117+Vikashsirvi@users.noreply.github.com>
2021-12-06 12:23:26 +0530

    Adding Dynamics Routes with Params

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Products from "./Pages/Products";
+import ProductDetails from "./Pages/ProductsDetails";
 import Welcome from "./Pages/Welcome";
 function App() {
   return (
     <div>
       <MainHeader />
       <Routes>
-        <Route exact path="/products" element={<Products />}></Route>
-        <Route exact path="/welcome" element={<Welcome />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/welcome" element={<Welcome />}></Route>
+        <Route
+          path="/product-details/:productId"
+          element={<ProductDetails />}
+        ></Route>
       </Routes>
     </div>
   );
