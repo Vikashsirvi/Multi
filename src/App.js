a59278cfc534d6982f86aa559ba98e48a693bfc8
vikash sirvi <50855117+Vikashsirvi@users.noreply.github.com>
2021-12-06 11:33:15 +0530

    Defining and using react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Routes>
+        <Route exact path="/products" element={<Products />}></Route>
+        <Route exact path="/welcome" element={<Welcome />}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+//our-domain.com/ => Component Welcome
+//our-domain.com/products => Component Products
