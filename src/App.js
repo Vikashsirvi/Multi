52eee5986ec5ea8c8a6080e8ae6657ab69b53154
vikash sirvi <50855117+Vikashsirvi@users.noreply.github.com>
2021-12-07 12:09:31 +0530

    onwards to a new project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" exact element={<Navigate to="/quotes" />}></Route>
+        <Route path="/quotes" element={<AllQuotes />}></Route>
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />}></Route>
+        <Route path="/new-quote" element={<NewQuote />}></Route>
+      </Routes>
     </div>
   );
 }
