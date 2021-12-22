426d583b86c14448919725a78528ebe05ecedc55
vikash sirvi <vikashsirvi9999@gmail.com>
2021-12-22 20:30:02 +0530

    adding user login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -19,38 +19,47 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     //optional: Add validation
+    let url;
     setIsLoading(true);
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDBtSm3xfDhajb7Lr0k_bPzwUrKW-zZto0";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDBtSm3xfDhajb7Lr0k_bPzwUrKW-zZto0",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDBtSm3xfDhajb7Lr0k_bPzwUrKW-zZto0";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          //...
+          return res.json();
         } else {
-          res.json().then((data) => {
+          return res.json().then((data) => {
             let errorMessage = "Authentication Failed";
             //show error model
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
   return (
     <section className={classes.auth}>
