b13be1b77d666db5506f6f23fa025fd31bd03623
vikash sirvi <vikashsirvi9999@gmail.com>
2021-12-27 12:13:19 +0530

    linking between page

--- pages/news/[detailPage].js
+++ pages/news/[detailPage].js
@@ -1,6 +1,15 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/something-important
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.detailPage;
+
+  //send a request to the backend api
+  //to fetch the news item with newsId
+
   return <h1>The Detail Page </h1>;
 }
 
