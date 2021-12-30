4c41ebbd9da4d21f46f87de19e749884a81bfa8a
vikash sirvi <vikashsirvi9999@gmail.com>
2021-12-30 11:41:26 +0530

    adding custom components & CSS Modules

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+function MeetupDetail() {
+  return (
+    <MeetupDetails
+      title="First Meetup"
+      image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Firishhistorypodcast.ie%2Fwp-content%2Fuploads%2F2014%2F08%2F2014-07-01-11.13.22-e1406902269265.jpg&f=1&nofb=1"
+      address="Some Street 5, Some City 404040"
+      desc="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetail;
