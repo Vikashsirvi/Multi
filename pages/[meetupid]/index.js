a4132334bffaf38383c82f303317aeb172ec972e
vikash sirvi <vikashsirvi9999@gmail.com>
2021-12-31 14:31:54 +0530

    working with params for ssg data fetching

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -4,11 +4,28 @@ function MeetupDetail() {
   return (
     <MeetupDetails
       title="First Meetup"
-      image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Firishhistorypodcast.ie%2Fwp-content%2Fuploads%2F2014%2F08%2F2014-07-01-11.13.22-e1406902269265.jpg&f=1&nofb=1"
+      image=""
       address="Some Street 5, Some City 404040"
       desc="This is a first meetup"
     />
   );
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "First Meetup",
+        image:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Firishhistorypodcast.ie%2Fwp-content%2Fuploads%2F2014%2F08%2F2014-07-01-11.13.22-e1406902269265.jpg&f=1&nofb=1",
+        address: "Some Street 5, Some City 404040",
+        desc: "This is a first meetup",
+      },
+    },
+  };
 }
 
 export default MeetupDetail;
