6c7a48d044cf7b589ce2a763ea752bb6612f4bcb
vikash sirvi <vikashsirvi9999@gmail.com>
2022-01-03 14:39:04 +0530

    working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,22 @@
+import { MongoClient } from "mongodb";
 // /api/new-meetup
 // POST
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://vikash:Vikash123@cluster0.f0bps.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne({ data });
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup Inserted" });
   }
 }
 
