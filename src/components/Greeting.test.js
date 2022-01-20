282167ffd3004f0b414ae79bc903aafb47cdfcd7
vikash sirvi <vikashsirvi9999@gmail.com>
2022-01-20 12:22:13 +0530

    Testing User Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting Component", () => {
@@ -12,5 +13,37 @@ describe("Greeting Component", () => {
     //Assert
     const helloWolrdElement = screen.getByText("Hello World", { exact: false });
     expect(helloWolrdElement).toBeInTheDocument();
+  });
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders changed if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //assert
+    const outbuttonElement = screen.getByText("Changed");
+    expect(outbuttonElement).toBeInTheDocument();
+  });
+  test("does not render good to see you", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //assert
+    const outbuttonElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outbuttonElement).toBeNull();
   });
 });
